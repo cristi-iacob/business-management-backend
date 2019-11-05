9558cd90d186e4d309e812ce2236ad09f5cf145b
Synneks <Deniseneotescu@gmail.com>
2019-11-05 11:05:57 +0200

    Changed project to maven

---
+++ src/main/java/ubb/proiectColectiv/businessmanagementbackend/BusinessManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package ubb.proiectColectiv.businessmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ubb/proiectColectiv/businessmanagementbackend/BusinessManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ubb.proiectColectiv.businessmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
