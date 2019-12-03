f1dd008208a7a98e2aa3315dc95d3a116481169b
Cristi Iacob <cristiiacob98@gmail.com>
2019-12-03 23:53:35 +0200

    Getting all users for a supervisor.
    Signed-off-by: Cristi Iacob <cristiiacob98@gmail.com>

--- src/main/java/ubb/proiectColectiv/businessmanagementbackend/controller/SupervisorController.java
+++ src/main/java/ubb/proiectColectiv/businessmanagementbackend/controller/SupervisorController.java
@@ -6,11 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ubb.proiectColectiv.businessmanagementbackend.service.SupervisorService;
+
+import java.util.List;
 
 @RestController
 public class SupervisorController {
@@ -41,9 +40,25 @@ public class SupervisorController {
         }
     }
 
-    @GetMapping(value = "/supervisor/approvals/{user}")
-    public ResponseEntity<String> get_Requests_Per_User() {
+    @GetMapping(value = "/supervisor/approvals/{id}")
+    public ResponseEntity<String> getRequestsPer_User() {
         return null;
+    }
+
+    @GetMapping(value = "/supervisor/{id}/users")
+    public ResponseEntity<String> getUsersForSupervisor(@PathVariable("id") String id) {
+        try {
+            logger.info("Getting all users for a supervisor");
+            List< String > users = supervisorService.getUsersForSupervisor(id);
+            if (users == null) {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+
+            return new ResponseEntity<>(objectMapper.writeValueAsString(users), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error at sending all user for supervisor" + e.getStackTrace());
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping(value = "/supervisor/setApproval")
