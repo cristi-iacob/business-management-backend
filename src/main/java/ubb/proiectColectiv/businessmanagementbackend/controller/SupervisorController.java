241abdd701fde9b8f162a5e4c39a84b077b17848
Cristi Iacob <cristiiacob98@gmail.com>
2019-12-03 22:04:33 +0200

    Added functionality for getting all unapproved users (for supervisor functionality usage).
    Signed-off-by: Cristi Iacob <cristiiacob98@gmail.com>

--- src/main/java/ubb/proiectColectiv/businessmanagementbackend/controller/SupervisorController.java
+++ src/main/java/ubb/proiectColectiv/businessmanagementbackend/controller/SupervisorController.java
@@ -1,6 +1,10 @@
 package ubb.proiectColectiv.businessmanagementbackend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +17,12 @@ public class SupervisorController {
 
     @Autowired
     SupervisorService supervisorService;
+    private ObjectMapper objectMapper;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public SupervisorController(SupervisorService supervisorService) {
         this.supervisorService = supervisorService;
+        objectMapper = new ObjectMapper();
     }
 
     @GetMapping(value = "/supervisor/approvals")
@@ -24,8 +31,14 @@ public class SupervisorController {
     }
 
     @GetMapping(value = "/supervisor/requests")
-    public ResponseEntity<String> get_Requests(@RequestBody String content) {
-        return null;
+    public ResponseEntity<String> getRequests() {
+        try {
+            logger.info("Sending all unapproved users!\n");
+            return new ResponseEntity<>(objectMapper.writeValueAsString(supervisorService.getPendingRequests()), HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error at sending all unapproved users!\n" + e.getStackTrace());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = "/supervisor/approvals/{user}")
--- src/main/java/ubb/proiectColectiv/businessmanagementbackend/service/SupervisorService.java
+++ src/main/java/ubb/proiectColectiv/businessmanagementbackend/service/SupervisorService.java
@@ -1,8 +1,29 @@
 package ubb.proiectColectiv.businessmanagementbackend.service;
 
 import org.springframework.stereotype.Service;
+import ubb.proiectColectiv.businessmanagementbackend.model.User;
+import ubb.proiectColectiv.businessmanagementbackend.utils.FirebaseUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class SupervisorService {
+	public List <String> getPendingRequests() {
+		List < String > retList = new ArrayList<>();
 
+		HashMap< String, Object > users = (HashMap) FirebaseUtils.getUpstreamData(Arrays.asList("User"));
+
+		for (String id : users.keySet()) {
+			HashMap < String, Object > attributes = (HashMap) FirebaseUtils.getUpstreamData(Arrays.asList("User", id));
+
+			if ((Boolean) attributes.get("approvedStatus") == false) {
+				retList.add((String) attributes.get("email"));
+			}
+		}
+
+		return retList;
+	}
 }
