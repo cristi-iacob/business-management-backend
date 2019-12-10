19197c3e34709b06c6baa054f4a3117f27e15b70
unknown <maris.mihaibogdan@gmail.com>
2019-12-10 03:34:09 +0200

    Top down service level functionality for fetching projects

---
+++ src/main/java/ubb/proiectColectiv/businessmanagementbackend/model/ProjectExperienceEntry.java
@@ -0,0 +1,27 @@
+package ubb.proiectColectiv.businessmanagementbackend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Class wrapping data for a project experience entry from the database.
+ * Also used as a transport model for the HTTP API.
+ * All dates are formatted as a UNIX style timestamp (millis since epoch start)
+ */
+@Getter
+@Setter
+@Builder
+public class ProjectExperienceEntry {
+    private int startDate;
+    private int endDate;
+    private String consultingLevel;
+    private String description;
+    private int projectEndDate;
+    private int projectStartDate;
+    private String projectName;
+    private String industry;
+    private String clientName;
+    private String clientAddress;
+}
--- src/main/java/ubb/proiectColectiv/businessmanagementbackend/service/UserService.java
+++ src/main/java/ubb/proiectColectiv/businessmanagementbackend/service/UserService.java
@@ -1,12 +1,15 @@
 package ubb.proiectColectiv.businessmanagementbackend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.var;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
+import ubb.proiectColectiv.businessmanagementbackend.model.ProjectExperienceEntry;
 import ubb.proiectColectiv.businessmanagementbackend.model.User;
 import ubb.proiectColectiv.businessmanagementbackend.utils.FirebaseUtils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -84,5 +87,79 @@ public class UserService {
             return FirebaseUtils.getUpstreamData(Arrays.asList("User", String.valueOf(Objects.hash(email))));
         }
         return "INVALID TOKEN";
+    }
+
+
+    /**
+     * Retrieves the project experience entries which match the specified email.
+     * @param email
+     * The email to be matched by the project experience entries.
+     * Identifies the owner of the requested entries.
+     * @return
+     * Collection of project experience entries which belong to the user identified by the provided email.
+     */
+    public List<ProjectExperienceEntry> getAllProjectExperienceEntriesForUserWithEmail(String email) {
+        var hashedEmail = Objects.hash(email);
+        return getProjectExperienceEntriesForEmailHash(hashedEmail);
+    }
+
+    /**
+     *  Dragi colegi imi pare rau pentru tigania asta dar nu stiu cum bunul si ocrotitorul Dumnezeu sa fac altfel
+     *  Va urez o infinitate de clipe frumoase alaturi de cei dragi (gen prieteni, familie si alte categorii de non-dusmani)
+     *
+     *            /^\/^\
+     *          _|__|  O|
+     * \/     /~     \_/ \
+     *  \____|__________/  \
+     *         \_______      \
+     *                 `\     \                 \
+     *                   |     |                  \
+     *                  /      /                    \
+     *                 /     /                       \\
+     *               /      /                         \ \
+     *              /     /                            \  \
+     *            /     /             _----_            \   \
+     *           /     /           _-~      ~-_         |   |
+     *          (      (        _-~    _--_    ~-_     _/   |
+     *           \      ~-____-~    _-~    ~-_    ~-_-~    /
+     *             ~-_           _-~          ~-_       _-~
+     *                ~--______-~                ~-___-~
+     */
+    private List<ProjectExperienceEntry> getProjectExperienceEntriesForEmailHash(int hashedEmail) {
+        var entries = new ArrayList<ProjectExperienceEntry>();
+        var projectExperiences = FirebaseUtils.getCollectionAsUpstreamData(Arrays.asList("ProjectExperience"), HashMap.class);
+        projectExperiences = projectExperiences.stream().filter(pe -> (Integer)pe.get("userId") == hashedEmail).collect(Collectors.toList());
+        for(var projectExperienceMap: projectExperiences) {
+            var consultingLevelId = (Integer)projectExperienceMap.get("consultingLevelId");
+            var projectId = (Integer)projectExperienceMap.get("projectId");
+
+            var consultingLevel = FirebaseUtils.getSingleAsUpstream(Arrays.asList("ConsultingLevel", consultingLevelId.toString()), String.class);
+            var project = FirebaseUtils.getSingleAsUpstream(Arrays.asList("Project", projectId.toString()), HashMap.class);
+
+            var clientId = (Integer)project.get("clientId");
+            var industryId = (Integer)project.get("industryId");
+
+            var client = FirebaseUtils.getSingleAsUpstream(Arrays.asList("Client", clientId.toString()), HashMap.class);
+            var industry = FirebaseUtils.getSingleAsUpstream(Arrays.asList("Industry", industryId.toString()), String.class);
+
+            var projectExperienceEntry = buildProjectExprienceEntry(projectExperienceMap, project, client, industry, consultingLevel);
+            entries.add(projectExperienceEntry);
+        }
+        return entries;
+    }
+
+    private ProjectExperienceEntry buildProjectExprienceEntry(Map<String, Object> entryFirstClassProperties, Map<String, Object> project, Map<String, Object> client, String industry, String consultingLevel) {
+        return ProjectExperienceEntry.builder()
+                .startDate((Integer)entryFirstClassProperties.get("startDate"))
+                .endDate((Integer)entryFirstClassProperties.get("endDate"))
+                .consultingLevel(consultingLevel)
+                .description((String)entryFirstClassProperties.get("description"))
+                .projectStartDate((Integer)project.get("startDate"))
+                .projectEndDate((Integer)project.get("endDate"))
+                .projectName((String)project.get("name"))
+                .industry(industry)
+                .clientAddress((String)client.get("address"))
+                .clientName((String)client.get("name"))
+                .build();
     }
 }
