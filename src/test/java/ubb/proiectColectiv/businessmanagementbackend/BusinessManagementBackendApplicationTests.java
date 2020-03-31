078585d051cc707fbbdfec6bd840c32a70091bff
notCosco <cosminlucaci98@gmail.com>
2020-03-31 23:06:42 +0300

    junit

--- src/test/java/ubb/proiectColectiv/businessmanagementbackend/BusinessManagementBackendApplicationTests.java
+++ src/test/java/ubb/proiectColectiv/businessmanagementbackend/BusinessManagementBackendApplicationTests.java
@@ -2,9 +2,11 @@ package ubb.proiectColectiv.businessmanagementbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ubb.proiectColectiv.businessmanagementbackend.model.User;
 
 @SpringBootTest
 class BusinessManagementBackendApplicationTests {
+
 
     @Test
     void contextLoads() {
---
+++ src/test/java/ubb/proiectColectiv/businessmanagementbackend/service/changeStrategies/DeleteSkillChangeStrategyTest.java
@@ -0,0 +1,21 @@
+package ubb.proiectColectiv.businessmanagementbackend.service.changeStrategies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteSkillChangeStrategyTest {
+
+    U
+    @Test
+    void canCompile() {
+
+
+    }
+
+    @Test
+    void preview() {
+
+
+    }
+}
